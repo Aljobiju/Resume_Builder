b85efd753e1013431911d0b8ec0782ba53d0115c
Aljobiju <aljovadackel@gmail.com>
2023-04-10 18:24:39 +0530

    Basic Structure

---
+++ src/Components/Header/Headerfile.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Header.css"
+
+function Headerfile() {
+  return (
+    <div className="header-container">
+      <header className="header">
+        <h1 className="header-title">RESUME BUILDER</h1>
+      </header>
+    </div>
+  )
+}
+
+export default Headerfile
